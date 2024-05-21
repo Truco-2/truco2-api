797a4a243bb2415c272263cd52bf15b547038d91
Nelson Neto <nelson16.code@gmail.com>
2024-05-20 23:18:33 -0400

    chore: add swagger

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Truco 2 API')
+        .setDescription(
+            'API for the Truco 2 application, see more in https://github.com/Truco-2',
+        )
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
