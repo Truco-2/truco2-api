9efae03323241153281a6d4771512abeb9ca026c
Nelson Neto <nelson16.code@gmail.com>
2024-05-20 20:42:34 -0400

    chore: add prisma

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World from the Truco 2 API!';
-  }
+    getHello(): string {
+        return 'Hello World from the Truco 2 API!';
+    }
 }
