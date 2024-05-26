242e9b24b822837a0b6f7ccb3ce9edfe7bddc666
icaroafonso594@gmail.com <icaroafonso594@gmail.com>
2024-05-26 14:30:30 -0400

    wip: add unit tests to users service

--- app/src/services/users/users.service.spec.ts
+++ app/src/services/users/users.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../../prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  
+  const mockPrismaService = {
+    user: {
+      findUnique: jest.fn(),
+      findFirst: jest.fn(),
+    },  
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,4 +29,6 @@ describe('UsersService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  
 });
