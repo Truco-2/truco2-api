e3154e59c455865565970b622c8955dc7df37ae3
icaroafonso594@gmail.com <icaroafonso594@gmail.com>
2024-05-26 17:37:02 -0400

    wip: add e2e tests to login

--- app/src/app.controller.ts
+++ app/src/app.controller.ts
@@ -1,21 +1,18 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
+
 import { LocalAuthGuard } from './auth/local/local-auth.guard';
 import { JwtAuthGuard } from './auth/jwt/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
     constructor(private authService: AuthService) { }
 
     @UseGuards(LocalAuthGuard)
-    @Post('auth/login')
+    @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user);
     }
+    //@UseGuards(JwtAuthGuard)
 
-    @UseGuards(JwtAuthGuard)
-    @Get('profile')
-    getProfile(@Request() req) {
-        return req.user;
-    }
 }
