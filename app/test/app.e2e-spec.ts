fc467c9e3ae54c9b6c1050bfd915875d2ce95c3f
icaroafonso594@gmail.com <icaroafonso594@gmail.com>
2024-05-26 20:42:17 -0400

    test: add e2e tests to app controller

--- app/test/app.e2e-spec.ts
+++ app/test/app.e2e-spec.ts
@@ -2,23 +2,55 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/app.module';
+import { AuthService } from 'src/auth/auth.service';
+import { LocalAuthGuard } from 'src/auth/local/local-auth.guard';
+import { emitWarning } from 'process';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
+    const mockAuthService = {
+        login: jest.fn(),
+        validateUser: jest.fn()
+    }
+
+
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
-        }).compile();
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: mockAuthService
+                }]
+        })
+            .overrideProvider(AuthService)
+            .useValue(mockAuthService)
+            .compile();
 
         app = moduleFixture.createNestApplication();
         await app.init();
     });
 
-    it('/ (POST)', () => {
+    it('/login (POST)', () => {
+        mockAuthService.login.mockReturnValue({
+            acess_token: "valid_token"
+        })
+
+        mockAuthService.validateUser.mockReturnValue({
+            id: 1,
+            email: "",
+            name: ""
+        })
+
+
         return request(app.getHttpServer())
             .post('/login')
-            .expect(200)
-            .expect('Hello World 2');
+            .send({
+                username: "user",
+                password: "password"
+            })
+            .expect(201)
+            .expect('{"acess_token":"valid_token"}');
     });
 });
