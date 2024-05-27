85eebd2c5c1212713664c729b2a4cce7b86c2c45
Nelson Neto <nelson16.code@gmail.com>
2024-05-27 03:11:45 -0400

    feat: add guard and socket.io types do room gateway

--- app/src/gateways/room/room.gateway.ts
+++ app/src/gateways/room/room.gateway.ts
@@ -1,15 +1,26 @@
+import { UseGuards } from '@nestjs/common';
 import {
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { LocalAuthGuard } from 'src/auth/local/local-auth.guard';
 
 @WebSocketGateway({ namespace: 'room' })
 export class RoomGateway {
+    constructor() {}
+
     @WebSocketServer() server;
 
+    @UseGuards(LocalAuthGuard)
+    handleConnection(client: Socket): void {
+        console.log(client.id);
+    }
+
+    @UseGuards(LocalAuthGuard)
     @SubscribeMessage('message')
-    handleMessage(client: any, message: string): void {
+    handleMessage(client: Socket, message: string): void {
         const msg = `${client.id} sends: ${message}`;
 
         this.server.emit('server-message', msg);
@@ -17,10 +28,10 @@ export class RoomGateway {
 
     @SubscribeMessage('enter-available-room-listing')
     handleEnterAvailableRoomListing(
-        client: any,
+        client: Socket,
         body: { gameRoomId: number },
     ): void {
-        client.join(body.gameRoomId);
+        client.join(body.gameRoomId.toString());
         this.server
             .to(body.gameRoomId)
             .emit(
