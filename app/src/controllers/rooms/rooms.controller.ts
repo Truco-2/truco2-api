8fdf93b96d1f6cf09c0999c36f6b490e01cb0010
Nelson Neto <nelson16.code@gmail.com>
2024-05-28 22:42:33 -0400

    feat: emit rooms via socket from created room

--- app/src/controllers/rooms/rooms.controller.ts
+++ app/src/controllers/rooms/rooms.controller.ts
@@ -8,16 +8,22 @@ import {
     UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { Room } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 import { FormatResponseInterceptor } from 'src/common/interceptors/format-response/format-response.interceptor';
+import { RoomGateway } from 'src/gateways/room/room.gateway';
 import { RoomsService } from 'src/services/rooms/rooms.service';
-import { IRoomResource } from 'src/types/rooms';
+import { RoomResourceDto } from 'src/types/room.dto';
 
+@ApiBearerAuth()
 @UseInterceptors(FormatResponseInterceptor)
 @Controller('rooms')
 export class RoomsController {
-    constructor(private roomsService: RoomsService) {}
+    constructor(
+        private roomsService: RoomsService,
+        private readonly roomGateway: RoomGateway,
+    ) {}
 
     @UseGuards(AuthGuard('jwt'))
     @Get('/')
@@ -29,9 +35,13 @@ export class RoomsController {
     @Post('/create')
     async create(
         @Body()
-        data: IRoomResource,
+        data: RoomResourceDto,
     ): Promise<Room> {
-        return await this.roomsService.create(data);
+        const room = await this.roomsService.create(data);
+
+        this.roomGateway.updateAvailableList(room);
+
+        return room;
     }
 
     @UseGuards(JwtAuthGuard)
