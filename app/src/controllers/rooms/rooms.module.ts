a743b3ddcefd2fcf8ded0341f50055e66feae80b
Nelson Neto <nelson16.code@gmail.com>
2024-05-28 22:42:33 -0400

    feat: emit rooms via socket from created room

--- app/src/controllers/rooms/rooms.module.ts
+++ app/src/controllers/rooms/rooms.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { RoomGateway } from 'src/gateways/room/room.gateway';
 import { PrismaService } from 'src/prisma.service';
 import { RoomsService } from 'src/services/rooms/rooms.service';
 
-@Module({ providers: [RoomsService, PrismaService] })
+@Module({ providers: [RoomsService, PrismaService, RoomGateway], imports: [] })
 export class RoomsModule {}
