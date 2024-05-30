509130af3c4d95a768c6b8d5a58a3bcbb7959cbb
Nelson Neto <nelson16.code@gmail.com>
2024-05-30 14:54:52 -0400

    feat: add valiadations for room service

--- app/src/common/filters/http-exception/http-exception.filter.ts
+++ app/src/common/filters/http-exception/http-exception.filter.ts
@@ -4,10 +4,12 @@ import {
     Catch,
     ExceptionFilter,
     HttpException,
+    UnauthorizedException,
 } from '@nestjs/common';
+import { isArray } from 'class-validator';
 import { Response } from 'express';
 
-@Catch(BadRequestException, HttpException)
+@Catch(BadRequestException, HttpException, Error, UnauthorizedException)
 export class HttpExceptionFilter<T> implements ExceptionFilter {
     catch(exception: T, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
@@ -17,15 +19,21 @@ export class HttpExceptionFilter<T> implements ExceptionFilter {
         const message = [];
 
         const badReq = exception as BadRequestException;
+        const errorReq = exception as Error;
 
         if (badReq['response']) {
-            if (badReq['response']['message']) {
+            if (
+                badReq['response']['message'] &&
+                isArray(badReq['response']['message'])
+            ) {
                 badReq['response']['message'].forEach((element) => {
                     message.push(element);
                 });
             }
 
             statusCode = badReq['response']['statusCode'] ?? statusCode;
+        } else if (errorReq.message) {
+            message.push(errorReq.message);
         }
 
         response.status(400).json({
