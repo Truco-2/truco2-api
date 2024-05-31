42c2ad734e507fb2de3db8c94547df2342d32067
Nelson Neto <nelson16.code@gmail.com>
2024-05-31 14:47:02 -0400

    test: update unit test imports

--- app/src/models/room/controllers/room.controller.spec.ts
+++ app/src/models/room/controllers/room.controller.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomController } from './room.controller';
+import { RoomModule } from '../room.module';
 
 describe('RoomController', () => {
     let controller: RoomController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            controllers: [RoomController],
+            imports: [RoomModule],
         }).compile();
 
         controller = module.get<RoomController>(RoomController);
