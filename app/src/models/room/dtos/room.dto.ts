7bf4eac2488a07c0a987431d0a3c1b7d1d1f7c3a
icaroafonso594@gmail.com <icaroafonso594@gmail.com>
2024-06-06 00:40:47 -0400

    feat: adjust creation rooms validation

--- app/src/models/room/dtos/room.dto.ts
+++ app/src/models/room/dtos/room.dto.ts
@@ -1,19 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import {
-    IsAlphanumeric,
     IsBoolean,
+    IsNotEmpty,
     IsNumber,
     IsString,
     Length,
+    Matches,
     Max,
     Min,
+    ValidateIf,
 } from 'class-validator';
 
 export class RoomResourceDto {
     @ApiProperty()
     @IsString()
-    @IsAlphanumeric()
+    @Matches(/^[a-zA-Z0-9.]*$/, {
+        message: 'Room must not have any special symbol',
+    })
     @Length(3, 20)
+    @Transform(({ value }) => value?.trim())
     name: string;
 
     @ApiProperty()
@@ -27,5 +33,8 @@ export class RoomResourceDto {
     isPrivate: boolean;
 
     @ApiProperty()
+    @ValidateIf((o) => o.isPrivate === true)
+    @Transform(({ value }) => value?.trim())
+    @IsNotEmpty({})
     password?: string;
 }
