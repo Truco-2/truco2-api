3a20b0b488d86702ea2fcdc915abf398d716d763
Nelson <nelson16.code@gmail.com>
2024-06-10 02:19:36 -0300

    Revert "feat: adjust creation rooms validation"
    This reverts commit 7bf4eac2488a07c0a987431d0a3c1b7d1d1f7c3a.

--- app/src/models/room/dtos/room.dto.ts
+++ app/src/models/room/dtos/room.dto.ts
@@ -1,25 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 import {
+    IsAlphanumeric,
     IsBoolean,
-    IsNotEmpty,
     IsNumber,
     IsString,
     Length,
-    Matches,
     Max,
     Min,
-    ValidateIf,
 } from 'class-validator';
 
 export class RoomResourceDto {
     @ApiProperty()
     @IsString()
-    @Matches(/^[a-zA-Z0-9.]*$/, {
-        message: 'Room must not have any special symbol',
-    })
+    @IsAlphanumeric()
     @Length(3, 20)
-    @Transform(({ value }) => value?.trim())
     name: string;
 
     @ApiProperty()
@@ -33,8 +27,5 @@ export class RoomResourceDto {
     isPrivate: boolean;
 
     @ApiProperty()
-    @ValidateIf((o) => o.isPrivate === true)
-    @Transform(({ value }) => value?.trim())
-    @IsNotEmpty({})
     password?: string;
 }
