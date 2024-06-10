2fc9b658e7501016156ddf82b0f405c3670c62cc
icaroafonso594@gmail.com <icaroafonso594@gmail.com>
2024-06-10 00:21:04 -0400

    feat: add endpoint to enter in a room and retrieve informations

--- app/src/models/room/dtos/room.dto.ts
+++ app/src/models/room/dtos/room.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
+import { Expose, Transform, Type } from 'class-transformer';
 import {
     IsBoolean,
     IsNotEmpty,
@@ -10,9 +10,37 @@ import {
     Max,
     Min,
     ValidateIf,
+    ValidateNested,
 } from 'class-validator';
+import exp from 'constants';
 
-export class RoomResourceDto {
+export class OwnerResource {
+    @Expose()
+    id: number;
+    @Expose()
+    name: string;
+}
+
+export class UserResource {
+    @Expose()
+    id: string;
+    @Expose()
+    name: UsersRoomsResource[];
+}
+
+export class UsersRoomsResource {
+    @Expose()
+    @Type(() => UserResource)
+    user: UserResource;
+}
+
+export class RoomDto {
+    @Expose()
+    id: number;
+
+    @Expose()
+    code: string;
+
     @ApiProperty()
     @IsString()
     @Matches(/^[a-zA-Z0-9.]*$/, {
@@ -20,21 +48,33 @@ export class RoomResourceDto {
     })
     @Length(3, 20)
     @Transform(({ value }) => value?.trim())
+    @Expose()
     name: string;
 
     @ApiProperty()
     @IsNumber()
     @Min(2)
     @Max(7)
+    @Expose()
     maxPlayers: number;
 
     @ApiProperty()
     @IsBoolean()
+    @Expose()
     isPrivate: boolean;
 
     @ApiProperty()
     @ValidateIf((o) => o.isPrivate === true)
     @Transform(({ value }) => value?.trim())
-    @IsNotEmpty({})
+    @IsNotEmpty()
     password?: string;
+
+    @Type(() => OwnerResource)
+    @Expose()
+    owner: OwnerResource;
+
+    @ValidateNested({ each: true })
+    @Type(() => UsersRoomsResource)
+    @Expose()
+    UsersRooms: UsersRoomsResource[];
 }
