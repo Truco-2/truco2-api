b93aa148aa5649c00d2e539563b6fe8229b6513c
Nelson Neto <nelson16.code@gmail.com>
2024-06-26 02:52:08 -0400

    test: why

--- app/src/modules/match/services/match.service.spec.ts
+++ app/src/modules/match/services/match.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchService } from './match.service';
+import { MatchModule } from '../match.module';
 
 describe('MatchService', () => {
     let service: MatchService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [MatchService],
+            imports: [MatchModule],
         }).compile();
 
         service = module.get<MatchService>(MatchService);
