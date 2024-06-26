4ba5355b1fe0a40788f83a8d1f1050171ebbbc7b
Nelson Neto <nelson16.code@gmail.com>
2024-06-26 02:03:47 -0400

    test: update match gateway unit tests

--- app/src/modules/match/gateways/match/match.gateway.spec.ts
+++ app/src/modules/match/gateways/match/match.gateway.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchGateway } from './match.gateway';
+import { MatchModule } from '../../match.module';
 
 describe('MatchGateway', () => {
     let gateway: MatchGateway;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [MatchGateway],
+            providers: [MatchModule],
         }).compile();
 
         gateway = module.get<MatchGateway>(MatchGateway);
