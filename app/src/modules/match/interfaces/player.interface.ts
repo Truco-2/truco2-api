99b0b4a366acb03770a68b4377a7bc6be79a41ca
Nelson Neto <nelson16.code@gmail.com>
2024-06-26 15:43:45 -0400

    feat: remove unsed interfaces

--- app/src/modules/match/interfaces/player.interface.ts
+++ app/src/modules/match/interfaces/player.interface.ts
@@ -1,5 +1,6 @@
 import { PlayerStatus, PlayerType } from 'src/common/enums/match.enum';
 import { User } from './user.interface';
+import { Play } from './play.interface';
 
 export interface Player {
     id: number;
@@ -7,8 +8,10 @@ export interface Player {
     user: User;
     type: PlayerType;
     bet: number | null;
+    wins: number;
     cardsOnNextRound: number;
     cardsOnHand: number;
     cards: number[];
     socketClientId: string | null;
+    play: Play | null;
 }
