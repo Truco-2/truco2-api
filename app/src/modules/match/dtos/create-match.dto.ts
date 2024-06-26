27d2b095a6f1a86eccf9fe4285ce2aa534fb8f91
Nelson Neto <nelson16.code@gmail.com>
2024-06-26 05:14:01 -0400

    feat: add enter in match gateway

--- app/src/modules/match/dtos/create-match.dto.ts
+++ app/src/modules/match/dtos/create-match.dto.ts
@@ -1,8 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
 
 export class CreateMatchDto {
     @ApiProperty()
-    @IsNumber()
     roomCode: string;
 }
