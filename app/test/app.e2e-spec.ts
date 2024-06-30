85edac5e11854d88c3924cf6d1ae7ab682ae4533
Nelson Neto <nelson16.code@gmail.com>
2024-06-29 21:58:05 -0400

    test: i hate icaro

--- app/test/app.e2e-spec.ts
+++ app/test/app.e2e-spec.ts
@@ -1,80 +1,62 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { INestApplication } from '@nestjs/common';
-// import { AppModule } from 'src/app.module';
-// import { AuthService } from 'src/auth/auth.service';
-// import { UserService } from 'src/modules/user/services/user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+import { AuthService } from 'src/auth/auth.service';
+import { UserService } from 'src/modules/user/services/user.service';
 
-// describe('AppController (e2e)', () => {
-//     let app: INestApplication;
+describe('AppController (e2e)', () => {
+    let app: INestApplication;
 
-//     const mockAuthService = {
-//         login: jest.fn(),
-//         validateUser: jest.fn(),
-//     };
+    const mockAuthService = {
+        login: jest.fn(),
+        validateUser: jest.fn(),
+    };
 
-//     const mockUserService = {
-//         generateGuestUser: jest.fn(),
-//     };
+    const mockUserService = {
+        generateGuestUser: jest.fn(),
+    };
 
-//     beforeEach(async () => {
-//         const moduleFixture: TestingModule = await Test.createTestingModule({
-//             imports: [AppModule],
-//             providers: [
-//                 {
-//                     provide: AuthService,
-//                     useValue: mockAuthService,
-//                 },
-//                 {
-//                     provide: UserService,
-//                     useValue: mockUserService,
-//                 },
-//             ],
-//         })
-//             .overrideProvider(AuthService)
-//             .useValue(mockAuthService)
-//             .compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+            providers: [
+                {
+                    provide: AuthService,
+                    useValue: mockAuthService,
+                },
+                {
+                    provide: UserService,
+                    useValue: mockUserService,
+                },
+            ],
+        })
+            .overrideProvider(AuthService)
+            .useValue(mockAuthService)
+            .compile();
 
-//         app = moduleFixture.createNestApplication();
-//         await app.init();
-//     });
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
 
-//     it('/login (POST)', () => {
-//         mockAuthService.login.mockReturnValue({
-//             acess_token: 'valid_token',
-//         });
+    it('/guest (GET)', () => {
+        const user = {
+            id: 1,
+            email: '',
+            name: '',
+        };
 
-//         mockAuthService.validateUser.mockReturnValue({
-//             id: 1,
-//             email: '',
-//             name: '',
-//         });
+        mockUserService.generateGuestUser.mockReturnValue(user);
+        mockAuthService.validateUser.mockReturnValue(user);
+        mockAuthService.login.mockReturnValue({
+            acess_token: 'valid_token',
+        });
 
-//         return request(app.getHttpServer())
-//             .post('/login')
-//             .send({
-//                 username: 'user',
-//                 password: 'password',
-//             })
-//             .expect(HttpStatus.CREATED)
-//             .expect('{"acess_token":"valid_token"}');
-//     });
-
-//     it('/guest (GET)', () => {
-//         const user = {
-//             id: 1,
-//             email: '',
-//             name: '',
-//         };
-
-//         mockUserService.generateGuestUser.mockReturnValue(user);
-//         mockAuthService.validateUser.mockReturnValue(user);
-//         mockAuthService.login.mockReturnValue({
-//             acess_token: 'valid_token',
-//         });
-
-//         return request(app.getHttpServer())
-//             .get('/guest')
-//             .expect(HttpStatus.OK)
-//             .expect('{"acess_token":"valid_token"}');
-//     });
-// });
+        return request(app.getHttpServer())
+            .get('/guest')
+            .expect(HttpStatus.OK)
+            .expect(
+                '{"success": true,"data": {"access_token": "valid_token"}}',
+            );
+    });
+});
